cbd183bff1e779a7f96f4313bddc7c6ddbf33888
parchmentEngineer <100083308+parchmentEngineer@users.noreply.github.com>
2022-07-28 13:20:56 -0400

    Added first three upgrades

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addLayer("p", {
+/*addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -25,4 +25,55 @@ addLayer("p", {
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
+})
+*/
+
+addLayer("s", {
+    name: "solar", 
+    symbol: "S", 
+    position: 0, 
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+		best: new Decimal(0),
+    }},
+    color: "#dff516",
+    requires: new Decimal(5), 
+    resource: "Solaris Essence",
+    baseResource: "points", 
+    baseAmount() {return player.points}, 
+    type: "normal",
+    exponent: 0.5,
+    gainMult() { 
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { 
+        return new Decimal(1)
+    },
+    row: 0,
+    hotkeys: [
+        {key: "s", description: "S: Reset for Solaris Essence", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+	upgrades: {
+		11: {
+			title: "Solar Rays",
+			description: "Multiply point gain by Sunlight amount.",
+			cost: new Decimal(1),
+		},
+		12: {
+			title: "Illumination",
+			description: "Multiply point gain by best Solaris Essence",
+			effect() {return player['s'].best},
+			//effectDisplay() {return upgradeEffect(this.layer, this.id)},
+			cost: new Decimal(5),
+		},
+		13: {
+			title: "Daybreak",
+			description: "Gain 1,000% of Solaris Essensce gain at 6:00 each day.",
+			effect() {return getResetGain(this.layer)*10},
+			cost: new Decimal(40),
+		},
+    },
 })
--- js/mod.js
+++ js/mod.js
@@ -7,7 +7,7 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new Decimal (5), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
@@ -41,8 +41,12 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
+	
+	updateTime()
 
 	let gain = new Decimal(1)
+	if (hasUpgrade('s', 11)) gain = gain.times(sunlight/100)
+	if (hasUpgrade('s', 12)) gain = gain.times(upgradeEffect('s', 12))
 	return gain
 }
 
@@ -50,8 +54,43 @@ function getPointGen() {
 function addedPlayerData() { return {
 }}
 
+let minutes = 0
+let hours = 6
+let sunlight = 100
+let moonlight = 100
+
+function updateTime() {
+	minutes = minutes+0.02;
+	if(minutes >= 6) {
+		minutes = 0
+		hours += 1
+		if (hours==6) {
+			if (hasUpgrade('s', 13)) {
+				player['s'].points = player['s'].points.add(upgradeEffect('s', 13))
+				if (player['s'].points.gt(player['s'].best)) { player['s'].best = player['s'].points }
+			}
+		}
+	}
+	if(hours >= 24) {
+		hours = 0
+	}
+	if(hours >= 6 && hours <= 17) {
+		sunlight = Math.floor(((36-Math.abs(12*6-((hours*6)+(Math.floor(minutes*100)/100))))/36)*100)+100
+		moonlight = 100
+	} else {
+		moonlight = Math.abs(Math.floor(((36-Math.abs(12*6-(((hours-12)*6)+(Math.floor(minutes*100)/100))))/36)*100)+100)
+		sunlight = 100
+	}
+}
+
 // Display extra things at the top of the page
 var displayThings = [
+	function() {
+		toreturn = "Time: "+hours+":"+Math.floor(minutes)+"0"
+		if(hours >= 6 && hours <= 17) {toreturn += " (Sunlight - "+sunlight+"%)"}
+		else {toreturn += " (Moonlight - "+moonlight+"%)"}
+		return toreturn
+	}
 ]
 
 // Determines when the game "ends"
